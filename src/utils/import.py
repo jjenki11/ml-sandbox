9c140240afeefcf25a25f6228924dbf93b6afbf4
jjenki11 <jeff.jenkins@activecanopy.com>
2018-12-09 23:24:09 -0500

    Added updates to import file

--- src/utils/import.py
+++ src/utils/import.py
@@ -7,8 +7,12 @@ from model.Entry import *
 
 import pandas as pd
 import numpy as np
-from tabulate import tabulate
+#from tabulate import tabulate
 import glob, os, pickle
+
+import csv
+
+
 
 
 
@@ -20,19 +24,22 @@ e.printEntry()
 ce = co2Entry({'c':'3','d':'4'})
 ce.printco2Entry()
 
-def PrintData(data):
-    print(tabulate(data, headers='keys', tablefmt='fancy_grid'))
-
-def CsvToDataset(filename):
-    return pd.read_csv(filename, header=0).dropna()
-
 from pathlib import Path
 project_dir = Path(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))).parent
-co2_per_person = str(project_dir)+"/data/co2_emissions_tonnes_per_person.csv"
-#print(os.path.dirname(os.path.dirname(os.path.abspath())))
 
-dataset = CsvToDataset(co2_per_person)
+data_dir = str(project_dir)+"/data/"
 
-PrintData(dataset)
+co2_per_person = data_dir + "co2_emissions_tonnes_per_person.csv"
 
 
+f = open(co2_per_person)
+
+co2_dict = {}
+
+data = []
+for line in f:
+    data_line = line.rstrip().split(',')
+    data.append(data_line)
+    co2_dict[data_line[0]] = data_line[2:-1]
+
+print(co2_dict)
